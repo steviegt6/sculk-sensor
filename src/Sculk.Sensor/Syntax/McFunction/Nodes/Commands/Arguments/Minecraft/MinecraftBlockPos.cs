4a4ce24c04774da40117006ebcd5f5ebed18c033
Tomat <xxlennygamerxx@gmail.com>
2023-04-01 16:06:54 -0700

    Rename Coordinates to BlockPosition.

--- src/Sculk.Sensor/Syntax/McFunction/Nodes/Commands/Arguments/Minecraft/MinecraftBlockPos.cs
+++ src/Sculk.Sensor/Syntax/McFunction/Nodes/Commands/Arguments/Minecraft/MinecraftBlockPos.cs
@@ -1,23 +1,23 @@
 ﻿namespace Sculk.Sensor.Syntax.McFunction.Nodes.Commands.Arguments.Minecraft;
 
-public readonly record struct Coordinates(
+public readonly record struct BlockPosition(
     Coordinate<int> X,
     Coordinate<int> Y,
     Coordinate<int> Z
 );
 
-public sealed class MinecraftBlockPos : IArgument<Coordinates> {
-    public Coordinates Value { get; }
+public sealed class MinecraftBlockPos : IArgument<BlockPosition> {
+    public BlockPosition Value { get; }
 
     public MinecraftBlockPos(
         Coordinate<int> x,
         Coordinate<int> y,
         Coordinate<int> z
     ) {
-        Value = new Coordinates(x, y, z);
+        Value = new BlockPosition(x, y, z);
     }
 
-    public static IArgument<Coordinates> Parse(string[] args, ref int index) {
+    public static IArgument<BlockPosition> Parse(string[] args, ref int index) {
         ArgConditions.AssertArgumentCount(args, index + 3);
         var x = ParseCoordinate(args[index++]);
         var y = ParseCoordinate(args[index++]);

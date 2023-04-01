d5baed332f755e16ed43a4e2ba88a572be984ab3
Tomat <xxlennygamerxx@gmail.com>
2023-04-01 16:16:31 -0700

    ResourceLocation.TryParse, MinecraftDimension.

--- src/Sculk.Sensor/Syntax/McFunction/Nodes/Commands/Arguments/Minecraft/ResourceLocation.cs
+++ src/Sculk.Sensor/Syntax/McFunction/Nodes/Commands/Arguments/Minecraft/ResourceLocation.cs
@@ -1,4 +1,15 @@
 ﻿namespace Sculk.Sensor.Syntax.McFunction.Nodes.Commands.Arguments.Minecraft;
 
 // TODO: Validate legal chars: a-z, 0-9, _, -, .
-public readonly record struct ResourceLocation(string Namespace, string Path);
+public readonly record struct ResourceLocation(string Namespace, string Path) {
+    public static bool TryParse(string input, out ResourceLocation rl) {
+        var parts = input.Split(':');
+        if (parts.Length != 2) {
+            rl = default;
+            return false;
+        }
+
+        rl = new ResourceLocation(parts[0], parts[1]);
+        return true;
+    }
+}

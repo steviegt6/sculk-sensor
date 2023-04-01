67d3fe86e86a013c26c487575aa48541421ce3fa
Tomat <xxlennygamerxx@gmail.com>
2023-04-01 16:14:00 -0700

    Iffy component/JSON support.

---
+++ src/Sculk.Sensor/Syntax/McFunction/Nodes/Commands/Arguments/Minecraft/MinecraftComponent.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Sculk.Sensor.Syntax.McFunction.Nodes.Commands.Arguments.Minecraft;
+
+public sealed class MinecraftComponent : IArgument<object> {
+    public object Value { get; }
+
+    public MinecraftComponent(object value) {
+        Value = value;
+    }
+
+    public static IArgument<object> Parse(string[] args, ref int index) {
+        ArgConditions.AssertArgumentCount(args, index + 1);
+        var json = args[index++];
+        var result = JsonConvert.DeserializeObject(json);
+        if (result is null)
+            throw new System.ArgumentException($"Expected a JSON object, but got {json}.");
+
+        return new MinecraftComponent(result);
+    }
+}
